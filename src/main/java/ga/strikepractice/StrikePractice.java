4d5489c4b63059b86816ee6f238899c367a0fa62
toppev <thetoppe5@gmail.com>
2019-06-26 12:51:19 +0300

    Add StrikePractice.java to get api

---
+++ src/main/java/ga/strikepractice/StrikePractice.java
@@ -0,0 +1,12 @@
+package ga.strikepractice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import ga.strikepractice.api.StrikePracticeAPI;
+
+public class StrikePractice extends JavaPlugin {
+
+    public static StrikePracticeAPI getAPI() {
+        return null;
+    }
+}
